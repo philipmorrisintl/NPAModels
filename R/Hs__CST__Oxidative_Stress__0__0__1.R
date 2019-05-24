3e4e1e5c0b491344466239cceae71ad68bc6a2d2
sgubianpm <sylvain.gubian@pmi.com>
2019-05-24 10:49:29 +0200

    Fixed typo in Oxidative stress

--- R/Hs__CST__Oxidative_Stress__0__0__1.R
+++ R/Hs__CST__Oxidative_Stress__0__0__1.R
@@ -3,9 +3,9 @@
 ## Quai Jeanrenaud 5, 2000 Neuchatel, Switzerland
 #####################################################################
 
-#' Oxydative Stress network for Homo sapiens (version 0.0.1)
+#' Oxidative Stress network for Homo sapiens (version 0.0.1)
 #'
-#' @name Hs__CST__Oxydative_Stress__0__0__1.rda
+#' @name Hs__CST__Oxidative_Stress__0__0__1.rda
 #' @docType data
 #' @keywords data
 NULL
--- R/Mm__CST__Oxidative_Stress__0__0__1.R
+++ R/Mm__CST__Oxidative_Stress__0__0__1.R
@@ -3,9 +3,9 @@
 ## Quai Jeanrenaud 5, 2000 Neuchatel, Switzerland
 #####################################################################
 
-#' Oxydative Stress network for Mus musculus (version 0.0.1)
+#' Oxidative Stress network for Mus musculus (version 0.0.1)
 #'
-#' @name Mm__CST__Oxydative_Stress__0__0__1.rda
+#' @name Mm__CST__Oxidative_Stress__0__0__1.rda
 #' @docType data
 #' @keywords data
 NULL
